c1dc41caa779c93bce6528f75aef3073840629a2
LucasCarvalhoAlmeida <lucas.almeida_bol@hotmail.com>
2025-01-21 00:05:10 +0000

    parte das entradas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,7 +18,32 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("Desafio Super Trunfo\n");
+    
+    int populacao;
+    float area;
+    float pib;
+    int pturismo;
+
+
+    printf("Desafio Super Trunfo Países\n");
+    printf("Cadastro das Cartas\n");
+
+    printf("Insira o número da população de seu País:\n");
+    scanf("%d", &populacao);
+    printf("População: %d\n", populacao);
+
+    printf("Insira a aréa de seu País:\n");
+    scanf("%f", &area);
+    printf("Aréa do seu País: %f\n", area);
+
+    printf("Insira o PIB do seu País:\n");
+    scanf("%f", &pib);
+    printf("PIB do seu País: %f\n", pib);
+
+    printf("Insira o número de pontos turistícos do seu País:\n");
+    scanf("%d", &pturismo);
+    printf("Pontos turisticos do seu Pais: %d\n", pturismo);
+
 
     return 0;
 }
